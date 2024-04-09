9dff76864dbb24a9f5d5fc4e21412e0ebb69cb9a
WorkingDada <6430222521@student.chula.ac.th>
2024-04-09 19:58:45 +0700

    Changed layout

--- frontend/src/app/store/ants/page.tsx
+++ frontend/src/app/store/ants/page.tsx
@@ -1,13 +1,11 @@
 import ItemCard from "@/components/Store/ItemCard";
-import MenuSidebar from "@/components/Store/MenuSidebar";
 
 const Home = () => {
   return (
     <div className="grid grid-cols-3 gap-4">
-      {/* <ItemCard />
       <ItemCard />
       <ItemCard />
-      <ItemCard /> */}
+      <ItemCard />
     </div>
   );
 };
--- frontend/src/components/Store/ItemCard.tsx
+++ frontend/src/components/Store/ItemCard.tsx
@@ -5,18 +5,18 @@ interface Props {
   detail : string
 }
 
-const ItemCard = ( data : Props) => {
+const ItemCard = () => {
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure>
         <img
-          src={data.src}
-          alt="item"
+          src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+          alt="Shoes"
         />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">{data.title}</h2>
-        <p>{data.detail}</p>
+        <h2 className="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
         <div className="card-actions justify-end">
           <button className="btn btn-primary">Buy Now</button>
         </div>
