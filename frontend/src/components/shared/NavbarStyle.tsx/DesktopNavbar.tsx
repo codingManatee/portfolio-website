197c2cdb87e65fff4b2ccee20e049fd1c67a9248
WorkingDada <6430222521@student.chula.ac.th>
2024-04-11 23:54:31 +0700

    WIP

--- frontend/src/components/shared/NavbarStyle.tsx/DesktopNavbar.tsx
+++ frontend/src/components/shared/NavbarStyle.tsx/DesktopNavbar.tsx
@@ -1,6 +1,6 @@
 const DesktopNavBar = () => {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-base-100 space-x-2">
       <div className="flex-none">
         <button className="btn btn-square btn-ghost">
           <svg
@@ -18,25 +18,27 @@ const DesktopNavBar = () => {
           </svg>
         </button>
       </div>
-      <div className="flex-1">
+      <div className="flex-1 space-x-2">
         <a className="btn btn-ghost text-xl">ExoticStock</a>
-      </div>
-      <div className="flex-none">
-        <button className="btn btn-square btn-ghost">
+        <label className="input input-bordered flex items-center gap-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="inline-block w-5 h-5 stroke-current"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="w-4 h-4 opacity-70"
           >
             <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-            ></path>
+              fillRule="evenodd"
+              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+              clipRule="evenodd"
+            />
           </svg>
-        </button>
+          <input type="text" className="grow" placeholder="Search" />
+        </label>
+      </div>
+      <div className="flex-none space-x-2">
+        <a className="btn">Login</a>
+        <a className="btn btn-neutral">Sign In</a>
       </div>
     </div>
   );
